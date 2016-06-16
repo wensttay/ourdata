9496f5e723b4c75a45c9c02d8eaceeec02cca093
Pedro Arthur <pfernandesvasconcelos@gmail.com>
2016-06-16 03:38:30 -0300

    fixing ResourceBdDao

--- src/main/java/br/ifpb/simba/ourdata/dao/entity/ResourceBdDao.java
+++ src/main/java/br/ifpb/simba/ourdata/dao/entity/ResourceBdDao.java
@@ -29,32 +29,36 @@ public class ResourceBdDao extends GenericBdDao
 {
     private PreparedStatement pstm;
     
-    public List<Resource> getResourcesIntersectedBy(Geometry g){
+    public List<Resource> getResourcesIntersectedBy(){
         List<Resource> resources = new ArrayList<>();
+        
+        String sql1 = "SELECT r.id, r.description, r.format, r.url, r.id_dataset, rp.repeat_number, rp.rows_number, rp.colum_value, rp.colum_name,\n" +
+            "rp.metadata_Created, rp.minX, rp.minY, rp.maxX, rp.maxY\n" +
+            "FROM Resource r JOIN Resource_Place rp ON r.id = rp.id_resource, (SELECT way FROM resource_place Where colum_value = 'Cajazeiras') c\n" +
+            "WHERE ST_Intersects(rp.way, c.way);";
+        
         String sql = "SELECT r.id, r.description, r.format, r.url, r.id_dataset, rp.repeat_number, rp.rows_number, rp.colum_number, rp.colum_name,\n" +
             "rp.metadataCreated, rp.id id_place, rp.nome, rp.sigla, rp.tipo, rp.minX, rp.minY, rp.maxX, rp.maxY\n" +
             "FROM Resource r JOIN Resource_Place rp ON r.id = rp.id_resource\n" +
             "WHERE ST_Intersects(rp.way, ?);";
         try
         {
+            conectar();
             WKTWriter writer = new WKTWriter();
-            PreparedStatement pstm = getConnection().prepareStatement(sql);
-            pstm.setString(1,writer.write(g));
+            PreparedStatement pstm = getConnection().prepareCall(sql1, ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+            //pstm.setString(1,writer.write(g));
             
             ResultSet rs = pstm.executeQuery();
                 
             Resource r;
             
             while(rs.next()){
-                
                 r = formaResource(rs);
                 resources.add(r);
-                
             }
             return resources;
         }
-        catch (URISyntaxException | IOException | SQLException | ClassNotFoundException ex)
-        {
+        catch (URISyntaxException | IOException | SQLException | ClassNotFoundException ex){
             Logger.getLogger(KeyPlaceBdDao.class.getName()).log(Level.SEVERE, null, ex);
         }
         return resources;
@@ -65,24 +69,21 @@ public class ResourceBdDao extends GenericBdDao
         String description = rs.getString("description");
         String format = rs.getString("format");
         String url = rs.getString("url");
-        String idDataset = rs.getString("idDataset");
+        String idDataset = rs.getString("id_Dataset");
         List<KeyPlace> keyplaces = new ArrayList<>();
         Place place = new Place();
         KeyPlace keyPlace = new KeyPlace();
         while(id_resource.equals(rs.getString("id"))){
-            place.setId(rs.getInt("id_place"));
             place.setMaxX(rs.getDouble("maxx"));
             place.setMaxY(rs.getDouble("maxy"));
             place.setMinX(rs.getDouble("minx"));
             place.setMinY(rs.getDouble("miny"));
-            place.setSigla(rs.getString("sigla"));
-            place.setTipo(rs.getString("tipo"));
+            
             keyPlace.setIdResource(id_resource);
             keyPlace.setPlace(place);
-            keyPlace.setColumNumber(rs.getInt("colum_number"));
             keyPlace.setColumValue(rs.getString("colum_value"));
-            keyPlace.setRowsNumber(rs.getInt("rowsNumber"));
-            keyPlace.setMetadataCreated(rs.getTimestamp("metaDataCreated"));
+            keyPlace.setRowsNumber(rs.getInt("rows_Number"));
+            keyPlace.setMetadataCreated(rs.getTimestamp("metaData_created"));
             keyPlace.setRepeatNumber(rs.getInt("repeat_number"));
             keyplaces.add(keyPlace);
             rs.next();
